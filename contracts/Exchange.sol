9b3795ae9611417fb1255ded4edf10b5c2f0b34d
HughesSeanKyle <khughessean@yahoo.com>
2023-01-11 18:53:35 +0200

    Add liquidity function

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -14,4 +14,87 @@ contract Exchange is ERC20 {
         );
         cryptoDevTokenAddress = _CryptoDevtoken;
     }
+
+    /**
+     * @dev Returns the amount of `Crypto Dev Tokens` held by the contract
+     */
+    function getReserve() public view returns (uint256) {
+        return ERC20(cryptoDevTokenAddress).balanceOf(address(this));
+    }
+
+    /**
+     * @dev Adds liquidity to the exchange.
+     */
+    function addLiquidity(uint256 _amount) public payable returns (uint256) {
+        uint256 liquidity;
+        uint256 ethBalance = address(this).balance;
+        uint256 cryptoDevTokenReserve = getReserve();
+        ERC20 cryptoDevToken = ERC20(cryptoDevTokenAddress);
+        /*
+        If the reserve is empty, intake any user supplied value for
+        `Ether` and `Crypto Dev` tokens because there is no ratio currently
+
+        The initial amount provided can be thought of as this => 10 A * 10 B = 100 (Lets say the reserve currency is USDT).
+        - The user comes and wants to buy 5 of Token A (Note token A is more expensive than token be. So 5 of AToken = 10 BToken) So the new pool balance is 5 ATok * 20 BTok = 100USDT
+        - How can this pool be balanced:  
+            - Arbitrage 
+            - Incentivize users to buy more of A in return for higher LP tokens
+                - (LP tokens can be traded for either protocol votes or other assets)
+            - Automatic rebalancing (If Atok falls below a cetain value then sell some of Btok to replenish Atok)
+            - Sliding scale LP token rewards based on ratios provided for pool
+                - e.g 5 LP tokens for 1:1 ratio, 10 LP tokens for 2:1, and 15 LP tokens for for a 3:1 ratio. 
+                - The above scales can be implemented based on the need for the assets and how many protocol tokens are left to back the LP tokens. LP tokens can also be backed by other assets held in reserve by the protocol. 
+                - Other incentives such as Governance tokens can also be offered as a reward   
+    */
+        if (cryptoDevTokenReserve == 0) {
+            // Transfer the `cryptoDevToken` from the user's account to the contract
+            cryptoDevToken.transferFrom(msg.sender, address(this), _amount);
+            // Take the current ethBalance and mint `ethBalance` amount of LP tokens to the user.
+            // `liquidity` provided is equal to `ethBalance` because this is the first time user
+            // is adding `Eth` to the contract, so whatever `Eth` contract has is equal to the one supplied
+            // by the user in the current `addLiquidity` call
+            // `liquidity` tokens that need to be minted to the user on `addLiquidity` call should always be proportional
+            // to the Eth specified by the user
+            liquidity = ethBalance;
+            _mint(msg.sender, liquidity);
+            // _mint is ERC20.sol smart contract function to mint ERC20 tokens
+        } else {
+            /*
+            If the reserve is not empty, intake any user supplied value for
+            `Ether` and determine according to the ratio how many `Crypto Dev` tokens
+            need to be supplied to prevent any large price impacts because of the additional
+            liquidity
+        */
+            // EthReserve should be the current ethBalance subtracted by the value of ether sent by the user
+            // in the current `addLiquidity` call
+            /*
+            Get the ethReserve state as at addLiquidity call 
+        */
+            uint256 ethReserve = ethBalance - msg.value;
+            // Ratio should always be maintained so that there are no major price impacts when adding liquidity
+            // Ratio here is -> (cryptoDevTokenAmount user can add/cryptoDevTokenReserve in the contract) = (Eth Sent by the user/Eth Reserve in the contract);
+            // So doing some maths, (cryptoDevTokenAmount user can add) = (Eth Sent by the user * cryptoDevTokenReserve /Eth Reserve);
+            uint256 cryptoDevTokenAmount = (msg.value * cryptoDevTokenReserve) /
+                (ethReserve);
+            require(
+                _amount >= cryptoDevTokenAmount,
+                "Amount of tokens sent is less than the minimum tokens required"
+            );
+            // transfer only (cryptoDevTokenAmount user can add) amount of `Crypto Dev tokens` from users account
+            // to the contract
+            cryptoDevToken.transferFrom(
+                msg.sender,
+                address(this),
+                cryptoDevTokenAmount
+            );
+            // The amount of LP tokens that would be sent to the user should be proportional to the liquidity of
+            // ether added by the user
+            // Ratio here to be maintained is ->
+            // (LP tokens to be sent to the user (liquidity)/ totalSupply of LP tokens in contract) = (Eth sent by the user)/(Eth reserve in the contract)
+            // by some maths -> liquidity =  (totalSupply of LP tokens in contract * (Eth sent by the user))/(Eth reserve in the contract)
+            liquidity = (totalSupply() * msg.value) / ethReserve;
+            _mint(msg.sender, liquidity);
+        }
+        return liquidity;
+    }
 }
